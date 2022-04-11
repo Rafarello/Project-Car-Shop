dd6532446114e24cab7ae3830bc4ddc842b13dcb
Rafael Moraes <rafael1moraes1rocha@gmail.com>
2022-04-11 16:11:37 -0300

    Requisito 1 - Mudando posição do export para passar no avaliador do lint que não considera os desativamentos inline

--- src/interfaces/ModelInterface.ts
+++ src/interfaces/ModelInterface.ts
@@ -1,15 +1,7 @@
-/* eslint-disable import/prefer-default-export */
-
-interface Model <T> {
+export interface Model <T> {
   create(obj: T): Promise<T>,
   read(): Promise<T[]>,
   readOne(id_: string): Promise<T | null>,
   update(): Promise<T[]>,
   delete(id: string): Promise<T | null>,
 }
-
-export {
-  Model,
-};
-
-/* eslint-enable import/prefer-default-export */
