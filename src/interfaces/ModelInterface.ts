c7314b245695b99b34c32004efbf41deb99e403b
Rafael Moraes <rafael1moraes1rocha@gmail.com>
2022-04-11 16:15:20 -0300

    Requisito 1 - Adicionando os argumentos que a função update da interface genérica de Model recebe

--- src/interfaces/ModelInterface.ts
+++ src/interfaces/ModelInterface.ts
@@ -1,7 +1,7 @@
 export interface Model <T> {
   create(obj: T): Promise<T>,
   read(): Promise<T[]>,
-  readOne(id_: string): Promise<T | null>,
-  update(): Promise<T[]>,
+  readOne(id: string): Promise<T | null>,
+  update(id: string, obj: T): Promise<T[]>,
   delete(id: string): Promise<T | null>,
 }
