1925937c04f87df9245ec643d9cfdb935479d3b0
Rafael Moraes <rafael1moraes1rocha@gmail.com>
2022-04-12 12:45:25 -0300

    Criado arquivo index e alguns comentários explicativos

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import server from './server';
+
+// Comando para iniciar a API
+// Mais informações se encontra no arquivo app.ts
+
+server.startServer();
--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,9 @@ import CarsRouter from './routers/CarsRouter';
 
 const server = new App();
 
+// Adicionando as rotas da API através da função addRouter
+// CarsRouter já foi importado como CarsRouter.router
+
 server.addRouter(CarsRouter);
 
 export default server;
