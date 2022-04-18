bbd3e1e02598cd0d058d56ad44123ad731351414
Rafael Moraes <rafael1moraes1rocha@gmail.com>
2022-04-18 19:32:15 -0300

    fix: Mudando o addrouter do arquivo index.ts para server.ts para passar no avaliador remoto e local

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,4 @@
 import server from './server';
-import CarsRouter from './routers/CarsRouter';
-
-// Adicionando as rotas da API através da função addRouter
-// CarsRouter já foi importado como CarsRouter.router
-
-server.addRouter(CarsRouter);
 
 // Comando para iniciar a API
 // Melhor visualização no arquivo app.ts
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,12 @@
 import App from './app';
 
+import CarsRouter from './routers/CarsRouter';
+
 const server = new App();
+
+// Adicionando as rotas da API através da função addRouter
+// CarsRouter já foi importado como CarsRouter.router
+
+server.addRouter(CarsRouter);
 
 export default server;
