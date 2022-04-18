8449f1c24be3de5cc1d1169624f1268d14502248
Rafael Moraes <rafael1moraes1rocha@gmail.com>
2022-04-18 17:29:56 -0300

    test: Implementando teste de cars. Teste de encontrar um carro específico por Id

--- src/tests/unit/models/CarModel.test.ts
+++ src/tests/unit/models/CarModel.test.ts
@@ -6,19 +6,23 @@ import CarsModel from '../../../models/CarsModel';
 import MockController from '../mock/MockController';
 
 describe('Teste unitário do model da rota /cars', () => {
-  // Mockando os Requests das funções do Model
-  const newCarRequest = MockController.mockGoodBodyRequest;
+
   // Mockando as Responses das funções do Model
   const newCarResponse = MockController.mockNewCar;
   const allcarsResponse = MockController.mockAllCars;
-
+  const findOneCarResponse = MockController.mockNewCar; 
+  
+  // Mockando os Requests das funções do Model
+  const newCarRequest = MockController.mockGoodBodyRequest;
+  const findOneCarRequest = findOneCarResponse._id;
+  
   // Criando uma nova instância do Model
   const CarModel = new CarsModel()
 
   before(async () => {
     sinon.stub(CarModel, "create").resolves(newCarResponse);
-    sinon.stub(CarModel, "read").resolves(allcarsResponse)
-
+    sinon.stub(CarModel, "read").resolves(allcarsResponse);
+    sinon.stub(CarModel, "readOne").resolves(findOneCarResponse)
   });
 
   after(() => {
@@ -39,9 +43,22 @@ describe('Teste unitário do model da rota /cars', () => {
 
   it('Deve ser possível pegar do banco de dados todas as informações de todos os carros', async () => {
     const allcars = await CarModel.read();
-    console.log(allcars);
     expect(allcars).to.deep.equal(allcarsResponse);
     expect(allcars).to.have.length(2);
   });
+
+  it('Deve ser possível pegar do banco de dados todas as informações de um carro específico', async () => {
+    const oneCar = await CarModel.readOne(findOneCarRequest);
+    expect(oneCar).to.deep.equal(findOneCarResponse);
+    expect(oneCar).to.have.property('model', 'Ferrari Maranello');
+    expect(oneCar).to.have.property('year', 1963);
+    expect(oneCar).to.have.property('color', 'red');
+    expect(oneCar).to.have.property('buyValue', 3500000);
+    expect(oneCar).to.have.property('doorsQty', 2);
+    expect(oneCar).to.have.property('seatsQty', 2);
+    expect(oneCar).to.have.property('_id', '62572a6880abf4bd240c0e52');
+  });
+
+  
 
 });
